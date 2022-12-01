5164669c9eed185de7363b9dd14ea0f8548aafa5
maycon <mayconxd11@gmail.com>
2022-12-01 16:42:30 -0300

    imcompleto-video 8

---
+++ blog-react-ex/src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css' ;
+
+function Home(){
+    return (
+        <>
+        <h1 className='titulo'>Home</h1>
+        <img src="https://static.vecteezy.com/ti/vetor-gratis/p1/5879568-web-designer-modern-flat-concept-for-web-banner-design-man-working-with-website-layout-places-images-and-other-graphic-elements-optimized-page-code-vector-illustration-cena-com-pessoas-isoladas-vetor.jpg" alt='imagem pessoas' className='foto'/>
+        </>
+
+    );
+}export default Home ;
